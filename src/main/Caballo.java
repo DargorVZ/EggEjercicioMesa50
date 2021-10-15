2d1055a1b5addad723f9806bb642560236868e27
Dario Vizcarra <DargorVZ@gmail.com>
2021-10-15 20:36:48 -0300

    Commit Caballo

--- src/main/Caballo.java
+++ src/main/Caballo.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
-/**
- *
- * @author Admin
- */
 public class Caballo {
+    private String nombre;
+    private int edad;
+
+    public Caballo() {
+    }
+
+    public Caballo(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
     
 }
